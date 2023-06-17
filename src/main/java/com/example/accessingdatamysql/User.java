a94313ce431dd63106e55428f64522b3e40219c4
rosauracotos <rosacotossullon@gmail.com>
2023-06-17 04:57:32 -0500

    subiendo proyecto con base de datos de mascotas

---
+++ src/main/java/com/example/accessingdatamysql/User.java
@@ -0,0 +1,51 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+
+  private String nombre;
+
+  private String raza;
+
+  private String propietario;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getRaza() {
+    return raza;
+  }
+
+  public void setRaza(String raza) {
+    this.raza = raza;
+  }
+
+  public String getPropietario() {
+    return propietario;
+  }
+
+  public void setPropietario(String propietario) {
+    this.propietario = propietario;
+  }
+}
